21ee76b03f457d8e87d4e2a5833ab89e9284222f
Jamie <jamieianwest@hotmail.com>
2019-07-17 14:51:14 +0100

    Adding IPv6 Auto Assign support as custom validation under AWSSubnet Added - New assign_ipv6_address_on_creation option that is checked in AWSSubnet only Fixed - Not defining global in terraguard.yaml broke validation

---
+++ test/test-aws/test_aws_resource_subnets.py
@@ -0,0 +1,64 @@
+from terraguard.resources.aws.aws_vpc import AWSDefaultSubnet, AWSSubnet
+
+
+def test_subnet_ipv6_rulesets():
+    rulesets = {'aws_subnet': [{
+        'expression': 'assign_ipv6_address_on_creation',
+        'must_equal': False
+    }]}
+
+    terraform_config = {
+        'address': 'someaddress',
+        'values': {
+            'tags': {'test': 'bleepbloop'},
+            'assign_ipv6_address_on_creation': True
+        }
+    }
+
+    subnet = AWSSubnet(terraform_config)
+    subnet.validate(rulesets)
+    assert len(subnet.violations) == 1
+    assert subnet.violations == {'someaddress.must_equal': ["(Incorrect value [Assign_ipv6_address_on_creation] must equal 'False' but found 'True'"]}
+
+    rulesets = {'aws_subnet': [{
+        'expression': 'assign_ipv6_address_on_creation',
+        'must_equal': False
+    }, {
+        'expression': 'tags',
+        'must_equal': {
+            'test': 'bleepbloop'
+        }
+    }]}
+
+    terraform_config = {
+        'address': 'someaddress',
+        'values': {
+            'tags': {'test': 'bleepbloop'},
+            'assign_ipv6_address_on_creation': True
+        }
+    }
+
+    subnet = AWSSubnet(terraform_config)
+    subnet.validate(rulesets)
+    assert len(subnet.violations) == 1
+    assert subnet.violations == {'someaddress.must_equal': ["(Incorrect value [Assign_ipv6_address_on_creation] must equal 'False' but found 'True'"]}
+
+
+def test_default_subnet_ipv6_rulesets():
+    rulesets = {'aws_default_subnet': [{
+        'expression': 'assign_ipv6_address_on_creation',
+        'must_equal': False
+    }]}
+
+    terraform_config = {
+        'address': 'someaddress',
+        'values': {
+            'tags': {'test': 'bleepbloop'},
+            'assign_ipv6_address_on_creation': True
+        }
+    }
+
+    subnet = AWSDefaultSubnet(terraform_config)
+    subnet.validate(rulesets)
+    assert len(subnet.violations) == 1
+    assert subnet.violations == {'someaddress.must_equal': ["(Incorrect value [Assign_ipv6_address_on_creation] must equal 'False' but found 'True'"]}
