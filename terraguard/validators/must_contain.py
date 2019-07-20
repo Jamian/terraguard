8c70c1eaf0ea582a6ccd3a01aeb693cdd4b0ce6e
Jamie <jamieianwest@hotmail.com>
2019-07-20 22:28:13 +0100

    Adding must_contain tests for 100% coverage

--- terraguard/validators/must_contain.py
+++ terraguard/validators/must_contain.py
@@ -20,33 +20,31 @@ def resource_must_contain(rule, resource, full_address):
 
 
 def attribute_must_contain(rule, resource, attribute_name, full_address):
-    error_message = None
     if isinstance(rule, str):
         if rule not in resource.config['values']:
             error_message = 'Missing required [{rule}] attribute from {resource_type}'.format(
                 rule=rule,
                 resource_type=resource.resource_type
             )
+            resource.add_violation(full_address, error_message)
     elif isinstance(rule, list):
         for item in rule:
             # If the attribute_name doesn't exist as all
             if attribute_name not in resource.config['values']:
-                if full_address not in resource.violations:
-                    resource.violations[full_address] = []
-                error_message = ['Missing {attribute_name} block and [{item}] required defined in {resource_type}'.format(
+                error_message = 'Missing {attribute_name} block and [{item}] required defined in {resource_type}'.format(
                     attribute_name=attribute_name.capitalize(),
                     item=item,
                     resource_type=resource.resource_type
-                )]
+                )
+                resource.add_violation(full_address, error_message)
             # If the attribute_name exists, but is set to a null value
             elif resource.config['values'][attribute_name] is None:
-                if full_address not in resource.violations:
-                    resource.violations[full_address] = []
-                error_message = ['Null {attribute_name} block and [{item}] required defined in {resource_type}'.format(
+                error_message = 'Null {attribute_name} block and [{item}] required defined in {resource_type}'.format(
                     attribute_name=attribute_name.capitalize(),
                     item=item,
                     resource_type=resource.resource_type
-                )]
+                )
+                resource.add_violation(full_address, error_message)
             # Lookup key exists so let's look at each individual item
             elif item not in resource.config['values'][attribute_name]:
                 error_message = 'Missing {attribute_name} [{item}] defined in {resource_type}'.format(
@@ -54,8 +52,7 @@ def attribute_must_contain(rule, resource, attribute_name, full_address):
                     item=item,
                     resource_type=resource.resource_type
                 )
-    if error_message:
-        resource.add_violation(full_address, error_message)
+                resource.add_violation(full_address, error_message)
 
 
 def must_contain(rule, resource, attribute_name=None):
